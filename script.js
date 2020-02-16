407112068b1cb3f326601d31b8b1d7e5f07533ab
Cramer <cramerm@nih.gov>
2020-02-16 17:34:39 -0500

    Adding functionality for the progress bar and  timestamp

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// documentation for video/audio api:
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Video_and_audio_APIs
+
 // get DOM elements
 const video = document.querySelector('#video');
 const play = document.querySelector('#play');
@@ -26,12 +29,34 @@ function updatePlayIcon() {
 
 // Update the progress and timestampe
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100; // assign a value of the current percentage complete
+  // the progress bar keeps moving because this event is constantly being called
+
+  // since this function constantly runs, we also want to update the clock here
+  // get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if(mins < 10) {
+    mins = '0' + String(mins);
+  }
+  // get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if(secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  // change the time on the page
+  timestamp.innerHTML = `${mins}:${secs}`;
+
+  
+  // console.log(video.currentTime); // keeps track of current time
+  // console.log(video.duration); // keeps track of total time
 }
 
 // Set video time to progress
+// set the current time of the video to be the same as the value progress multipled by video duration divided by 100
+// the + in front of progress.value makes sure its a number
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop the video
